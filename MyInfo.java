95c76d085d4a2ae265b0297ba798bd3d306cb7eb
andywong63 <andyw63@nycstudents.net>
2024-10-02 15:34:33 +0000

    Add code

---
+++ MyInfo.java
@@ -0,0 +1,12 @@
+/* This class shows information about me
+   Name: Andy */
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Andy."); // Name
+        System.out.println(); // Blank line
+        System.out.println("I am 15 years old."); // Age
+        System.out.print("I have ");
+        System.out.print(5 * 3 - 13);
+        System.out.println(" brothers."); // Number of brothers
+    }
+}
---
+++ Unit1Topic1.java
@@ -0,0 +1,8 @@
+public class Unit1Topic1 {
+    public static void main(String[] args) {
+        System.out.println(5 * 3);
+        System.out.println(10 / 5);
+        System.out.println(23 + 5);
+        System.out.println(7 - 10);
+    }
+}
